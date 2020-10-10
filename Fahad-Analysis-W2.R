45e3d8729da23e7cca073cd57d43a4bdb5381354
Fahad Masood Reda <fahad.m.reda@hotmail.com>
2020-10-10 05:08:40 +0300

    I analyzed the insurance dataset

---
+++ Fahad-Analysis-W2.R
@@ -0,0 +1,9 @@
+# Insuarance Analysis
+#Dataset URL: https://www.kaggle.com/mirichoi0218/insurance
+# Fahad Reda
+# 09.10.2020
+# Load packages
+library(tidyverse)
+
+# Read in the data
+df <- read_csv("insurance.csv")
